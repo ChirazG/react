468382cff75d38b9239321ee883f4a97aaad4aee
Chiraz GOUISSEM <chirazgouissem@gmail.com>
2021-03-25 04:12:02 +0100

    add react

---
+++ myapp/src/App.js
@@ -0,0 +1,93 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button,Form,Col,Navbar } from 'react-bootstrap';
+import ReactStars from "react-rating-stars-component";
+
+function App() {
+  const ratingChanged = (newRating) => {
+    console.log(newRating);
+  };
+  return (
+    
+    <div className="App">
+  <Navbar>
+    <h1 className="form">Form</h1>
+  </Navbar>
+  <br />
+      <Form>
+  <Form.Row>
+    <Col>
+      <Form.Control placeholder="First name" />
+    </Col>
+    <Col>
+      <Form.Control placeholder="Last name" />
+    </Col>
+  </Form.Row>
+</Form>
+      <Form>
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+
+</Form>
+<Form>
+<Form.Group controlId="formGridAddress1">
+    <Form.Label>Address</Form.Label>
+    <Form.Control placeholder="1234 Main St" />
+  </Form.Group>
+
+  <Form.Row>
+    <Form.Group as={Col} controlId="formGridCity">
+      <Form.Label>City</Form.Label>
+      <Form.Control />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridState">
+      <Form.Label>State</Form.Label>
+      <Form.Control as="select" defaultValue="Choose...">
+        <option>Choose...</option>
+        <option>...</option>
+      </Form.Control>
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridZip">
+      <Form.Label>Zip</Form.Label>
+      <Form.Control />
+    </Form.Group>
+  </Form.Row>
+
+  <Form.Group id="formGridCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+<br/>
+<h6>Don't forget to rate us!</h6>
+<ReactStars
+    count={5}
+    onChange={ratingChanged}
+    size={24}
+    isHalf={true}
+    emptyIcon={<i className="far fa-star"></i>}
+    halfIcon={<i className="fa fa-star-half-alt"></i>}
+    fullIcon={<i className="fa fa-star"></i>}
+    activeColor="#ffd700"
+  />
+    </div>
+  );
+}
+
+export default App;
